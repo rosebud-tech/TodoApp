60e23b84659534513289660632816f68e4ba605b
rosebud-tech <ogbedon@yahoo.com>
2021-06-21 18:01:50 +0100

    initial commit

---
+++ app/src/main/java/com/nwanneka/todoapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nwanneka.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val Username: String=""
+    val password: String=""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
