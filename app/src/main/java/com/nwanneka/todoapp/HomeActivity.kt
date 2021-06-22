9d05bb949b3d8eccd9dc69cc5b15708eb24d7bcf
rosebud-tech <ogbedon@yahoo.com>
2021-06-22 17:25:28 +0100

    second commit

---
+++ app/src/main/java/com/nwanneka/todoapp/HomeActivity.kt
@@ -0,0 +1,17 @@
+package com.nwanneka.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var usernamedisplay: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        usernamedisplay= findViewById(R.id.usernamedisplay)
+        usernamedisplay.text=intent.getStringExtra("username")
+
+    }
+
+}
